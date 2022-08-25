5362e9d98fa90ed5fdcf11e70389fba410410735
mburu3onesmus9kamau <onesmus.mburu@student.moringaschool.com>
2022-08-25 02:00:26 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arrayOfNames ,events){
+let thankYouArray=[]
+    for(let i=0; i<arrayOfNames.length;i++){
+        let messages =`thank you, ${arrayOfNames[i]},for the wonderful ${event}gift!`
+        thankYouArray.push(messages)
+    }
+    return thankYouArray(messages)
+}
+function countDown(initialNumber){
+    while(initialNumber>0){
+        console.log(initialNumber);
+        initialNumber=initialNumber-1
+    }
+    console.log (initialNumber)
+}
